fe4b7bb1800ba007515280317bfac092e0fd4a7e
Senthan <senthanganesh5885@gmail.com>
2025-09-08 11:31:07 +0530

    delete readme

--- src/main/java/com/senthan/shopeasy/Controller/ImageController.java
+++ src/main/java/com/senthan/shopeasy/Controller/ImageController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +34,7 @@ public class ImageController {
         }
     }
 
+    @GetMapping("image/download/{imageId}")
     public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
         Image image = iImageService.getImageById(imageId);
         ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1,(int) image.getImage().length()));
